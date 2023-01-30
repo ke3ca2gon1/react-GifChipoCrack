ba650c286d21fde4d2e928f2edb8f32dd9f6982d
Italo Lopez <lopezyslaitalogonzalo2712@gmail.com>
2023-01-30 09:29:43 -0500

    actualizadaso

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,12 +1,12 @@
 
 
-export const getGifs = async (category) => {
+export const getGifs = async ( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=5K8y0JP4QeAn3zyXqF2DAxDDEz7RxosR`;
-    const resp = await fetch(url);
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=5K8y0JP4QeAn3zyXqF2DAxDDEz7RxosR`;
+    const resp = await fetch( url );
     const { data } = await resp.json();
 
-    const gifs = data.map(img => {
+    const gifs = data.map( img => {
         return {
             id: img.id,
             title: img.title,
